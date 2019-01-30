93b6da9007f4999403f1da3c71cbd45fd38f13cf
Kamil Osuch <kamil.osuch@gmail.com>
2019-01-30 23:43:32 +0100

    Move key management to KeyEventManager

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -23,11 +23,8 @@ public class EventManager : MonoBehaviour
     private Dictionary<string, Action> _eventDictionary;
 
     private static EventManager _eventManager;
-
-    private Dictionary<KeyRepresentation, float> _buttonsDown = new Dictionary<KeyRepresentation, float>();
-    private Dictionary<KeyRepresentation, Tuple<string, string>> _keyCodeAliases = new Dictionary<KeyRepresentation, Tuple<string, string>>();
+    private KeyEventManager _keyEventManager = new KeyEventManager();
     
-
     public static EventManager Instance
     {
         get
@@ -70,14 +67,11 @@ public class EventManager : MonoBehaviour
                     continue;
                 }
 
-                if (!LogAllKeys && !_keyCodeAliases.ContainsKey(KeyRepresentation.FromEvent(myEvent)))
+                if (!LogAllKeys && !_keyEventManager.HasKeyAlias(myEvent))
                 {
                     continue;
                 }
-
-//                Debug.Log(myEvent.rawType);
-
-                RecognizeKeyEvent(myEvent, time);
+                _keyEventManager.RecognizeKeyEvent(myEvent, time);
             }
             else if (myEvent.isMouse)
             {
@@ -101,88 +95,6 @@ public class EventManager : MonoBehaviour
         Debug.Log(_event.keyCode);
     }
 
-    private void RecognizeKeyEvent(Event _event, float time)
-    {
-        KeyRepresentation keyRepresentation = KeyRepresentation.FromEvent(_event);
-        if (_event.rawType == EventType.KeyDown)
-        {
-            if (_buttonsDown.ContainsKey(keyRepresentation)) return;
-            _buttonsDown.Add(keyRepresentation, time);
-            LogEvent(time, GetEventSettings(keyRepresentation, _event.rawType.ToString()));
-        }
-        else if (_event.rawType == EventType.KeyUp)
-        {
-            if (IsSingleDownModifierKey(_event))
-            {
-                var representationKeys = GetModifiedRepresentationKeys(_event);
-                representationKeys.Add(keyRepresentation);
-                representationKeys.ForEach(representation => LogKeyDown(_event, time, representation));
-            }
-            else
-            {
-                LogKeyDown(_event, time, keyRepresentation);
-            }
-        }
-    }
-
-    private List<KeyRepresentation> GetModifiedRepresentationKeys(Event _event)
-    {
-        return _buttonsDown.Keys
-            .Where(representation => representation.WasModifiedByKey(_event.keyCode)).ToList();
-    }
-
-    private bool IsSingleDownModifierKey(Event _event)
-    {
-        if (!(_event.keyCode == KeyCode.LeftAlt || _event.keyCode == KeyCode.RightAlt ||
-              _event.keyCode == KeyCode.LeftControl || _event.keyCode == KeyCode.RightControl ||
-              _event.keyCode == KeyCode.LeftShift || _event.keyCode == KeyCode.RightShift))
-        {
-            return false;
-        }
-        
-        return !_buttonsDown.Keys.Any(representation =>
-        {
-            switch (_event.keyCode)
-            {
-                case KeyCode.LeftAlt:
-                    return representation.KeyCode.Equals(KeyCode.RightAlt);
-                case KeyCode.RightAlt:
-                    return representation.KeyCode.Equals(KeyCode.LeftAlt);
-                case KeyCode.LeftControl:
-                    return representation.KeyCode.Equals(KeyCode.RightControl);
-                case KeyCode.RightControl:
-                    return representation.KeyCode.Equals(KeyCode.LeftControl);
-                case KeyCode.LeftShift:
-                    return representation.KeyCode.Equals(KeyCode.RightShift);
-                case KeyCode.RightShift:
-                    return representation.KeyCode.Equals(KeyCode.LeftShift);
-                default:
-                    return false;
-            }
-        });
-        
-    }
-
-    private void LogKeyDown(Event _event, float time, KeyRepresentation keyRepresentation)
-    {
-        if (_buttonsDown.ContainsKey(keyRepresentation))
-        {
-            var holdTime = time - _buttonsDown[keyRepresentation];
-            if (holdTime < 0.2f)
-            {
-                LogEvent(time, GetEventSettings(keyRepresentation, _event.rawType.ToString()));
-            }
-            else
-            {
-                LogEvent(time,
-                    GetEventSettings(keyRepresentation, _event.rawType.ToString())
-                        .Attribute("HoldTime", holdTime));
-            }
-        }
-
-        _buttonsDown.Remove(keyRepresentation);
-    }
-
     private void Init()
     {
         if (_eventDictionary == null)
@@ -193,16 +105,7 @@ public class EventManager : MonoBehaviour
 
     public static void AddKeyAlias(KeyRepresentation key, string tag, string aliasName)
     {
-        Instance._keyCodeAliases.Add(key, Tuple.Create(tag, aliasName));
-    }
-
-    private EventSettings.EventSettingsBuilder GetEventSettings(KeyRepresentation key, string type)
-    {
-        var tuple = _keyCodeAliases.ContainsKey(key)
-            ? _keyCodeAliases[key]
-            : Tuple.Create("Player", key.ToString());
-        
-        return EventSettings.Builder(tuple.Item1, type, tuple.Item2);
+        Instance._keyEventManager.AddKeyAlias(key, tag, aliasName);
     }
 
     public static void LogEvent(float time, string tag, string type, string eventName, string attributeName = null,
---
+++ Assets/Scripts/KeyEventManager.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class KeyEventManager
+{
+    
+    private Dictionary<KeyRepresentation, float> _buttonsDown = new Dictionary<KeyRepresentation, float>();
+    private Dictionary<KeyRepresentation, Tuple<string, string>> _keyCodeAliases = new Dictionary<KeyRepresentation, Tuple<string, string>>();
+
+
+    public void RecognizeKeyEvent(Event _event, float time)
+    {
+        KeyRepresentation keyRepresentation = KeyRepresentation.FromEvent(_event);
+        if (_event.rawType == EventType.KeyDown)
+        {
+            if (_buttonsDown.ContainsKey(keyRepresentation)) return;
+            _buttonsDown.Add(keyRepresentation, time);
+            EventManager.LogEvent(time, GetEventSettings(keyRepresentation, _event.rawType.ToString()));
+        }
+        else if (_event.rawType == EventType.KeyUp)
+        {
+            if (IsSingleDownModifierKey(_event))
+            {
+                var representationKeys = GetModifiedRepresentationKeys(_event);
+                representationKeys.Add(keyRepresentation);
+                representationKeys.ForEach(representation => LogKeyDown(_event, time, representation));
+            }
+            else
+            {
+                LogKeyDown(_event, time, keyRepresentation);
+            }
+        }
+    }
+
+    public bool HasKeyAlias(Event myEvent)
+    {
+        return _keyCodeAliases.ContainsKey(KeyRepresentation.FromEvent(myEvent));
+    }
+    
+    private List<KeyRepresentation> GetModifiedRepresentationKeys(Event _event)
+    {
+        return _buttonsDown.Keys
+            .Where(representation => representation.WasModifiedByKey(_event.keyCode)).ToList();
+    }
+
+    private bool IsSingleDownModifierKey(Event _event)
+    {
+        if (!(_event.keyCode == KeyCode.LeftAlt || _event.keyCode == KeyCode.RightAlt ||
+              _event.keyCode == KeyCode.LeftControl || _event.keyCode == KeyCode.RightControl ||
+              _event.keyCode == KeyCode.LeftShift || _event.keyCode == KeyCode.RightShift))
+        {
+            return false;
+        }
+        
+        return !_buttonsDown.Keys.Any(representation =>
+        {
+            switch (_event.keyCode)
+            {
+                case KeyCode.LeftAlt:
+                    return representation.KeyCode.Equals(KeyCode.RightAlt);
+                case KeyCode.RightAlt:
+                    return representation.KeyCode.Equals(KeyCode.LeftAlt);
+                case KeyCode.LeftControl:
+                    return representation.KeyCode.Equals(KeyCode.RightControl);
+                case KeyCode.RightControl:
+                    return representation.KeyCode.Equals(KeyCode.LeftControl);
+                case KeyCode.LeftShift:
+                    return representation.KeyCode.Equals(KeyCode.RightShift);
+                case KeyCode.RightShift:
+                    return representation.KeyCode.Equals(KeyCode.LeftShift);
+                default:
+                    return false;
+            }
+        });
+        
+    }
+
+    private void LogKeyDown(Event _event, float time, KeyRepresentation keyRepresentation)
+    {
+        if (_buttonsDown.ContainsKey(keyRepresentation))
+        {
+            var holdTime = time - _buttonsDown[keyRepresentation];
+            if (holdTime < 0.2f)
+            {
+                EventManager.LogEvent(time, GetEventSettings(keyRepresentation, _event.rawType.ToString()));
+            }
+            else
+            {
+                EventManager.LogEvent(time,
+                    GetEventSettings(keyRepresentation, _event.rawType.ToString())
+                        .Attribute("HoldTime", holdTime));
+            }
+        }
+
+        _buttonsDown.Remove(keyRepresentation);
+    }
+    
+    private EventSettings.EventSettingsBuilder GetEventSettings(KeyRepresentation key, string type)
+    {
+        var tuple = _keyCodeAliases.ContainsKey(key)
+            ? _keyCodeAliases[key]
+            : Tuple.Create("Player", key.ToString());
+        
+        return EventSettings.Builder(tuple.Item1, type, tuple.Item2);
+    }
+
+    public void AddKeyAlias(KeyRepresentation key, string tag, string aliasName)
+    {
+        _keyCodeAliases.Add(key, Tuple.Create(tag, aliasName));
+    }
+}
