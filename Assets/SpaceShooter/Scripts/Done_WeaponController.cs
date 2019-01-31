040a80b4fc84421cfe314437874b8d483918449b
Kamil Osuch <kamil.osuch@gmail.com>
2019-01-31 16:51:21 +0100

    Add space shooter example

---
+++ Assets/SpaceShooter/Scripts/Done_WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_WeaponController : MonoBehaviour
+{
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	public float delay;
+
+	void Start ()
+	{
+		InvokeRepeating ("Fire", delay, fireRate);
+	}
+
+	void Fire ()
+	{
+		EventManager.LogEvent(Time.time, tag, "PlayerControl", "Shoot", "PositionX", shotSpawn.position.x);
+		EventManager.LogEvent(Time.time, tag, "PlayerControl", "Shoot", "PositionY", shotSpawn.position.y);
+		Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		GetComponent<AudioSource>().Play();
+	}
+}
